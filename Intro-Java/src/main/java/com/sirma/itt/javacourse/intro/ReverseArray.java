7b42a2b490223682e167482e20fe980697bc650d
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-04 15:53:30 +0300

    Added array reverse task

---
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/ReverseArray.java
@@ -0,0 +1,32 @@
+package com.sirma.itt.javacourse.intro;
+
+/**
+ * Class with method for reversing the elements in array of integers.
+ * 
+ * @author smustafov
+ */
+public final class ReverseArray {
+
+	/**
+	 * Protection from instantiation.
+	 */
+	private ReverseArray() {
+
+	}
+
+	/**
+	 * Method for reversing the elements in array of integers.
+	 * 
+	 * @param arr
+	 *            the array to be reversed
+	 */
+	public static void reverse(int[] arr) {
+		int length = arr.length;
+		for (int index = 0; index < length / 2; index++) {
+			int temp = arr[index];
+			arr[index] = arr[length - 1 - index];
+			arr[length - 1 - index] = temp;
+		}
+	}
+
+}
