1596bf6209854a95e049c89f533a6e946de4665e
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-04 18:20:09 +0300

    Added comments

--- Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/numbers/LargeNumbers.java
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/numbers/LargeNumbers.java
@@ -2,8 +2,29 @@ package com.sirma.itt.javacourse.intro.numbers;
 
 import java.math.BigInteger;
 
+/**
+ * Class for work with large numbers.
+ * 
+ * @author smustafov
+ */
 public final class LargeNumbers {
 
+	/**
+	 * Protection from instantiation.
+	 */
+	private LargeNumbers() {
+
+	}
+
+	/**
+	 * Sum two large numbers.
+	 * 
+	 * @param number1AsString
+	 *            the first number to sum
+	 * @param number2AsString
+	 *            the second number to sum
+	 * @return number1AsString + number2AsString
+	 */
 	public static BigInteger sumTwoNumbers(String number1AsString, String number2AsString) {
 		BigInteger number1 = new BigInteger(number1AsString);
 		BigInteger number2 = new BigInteger(number2AsString);
