21eb7ce1531d401a24acb3f653c43535bb5037a6
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-07 12:58:38 +0300

    Add task - sum large numbers

--- Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/numbers/LargeNumbers.java
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/numbers/LargeNumbers.java
@@ -26,9 +26,40 @@ public final class LargeNumbers {
 	 * @return number1AsString + number2AsString
 	 */
 	public static BigInteger sumTwoNumbers(String number1AsString, String number2AsString) {
-		BigInteger number1 = new BigInteger(number1AsString);
-		BigInteger number2 = new BigInteger(number2AsString);
-		return number1.add(number2);
-	}
+		StringBuilder result = new StringBuilder();
+		int numberWithMaxLen = Math.max(number1AsString.length(), number2AsString.length());
+		int digit1;
+		int digit2;
+		int sum = 0;
+		int carry = 0;
+		for (int i = 0; i < numberWithMaxLen; i++) {
+			if (i < number1AsString.length()) {
+				digit1 = Character.getNumericValue(number1AsString.charAt(i));
+			} else {
+				digit1 = 0;
+			}
 
+			if (i < number2AsString.length()) {
+				digit2 = Character.getNumericValue(number2AsString.charAt(i));
+			} else {
+				digit2 = 0;
+			}
+
+			sum = digit1 + digit2 + carry;
+			if (sum >= 10) {
+				carry = sum;
+				sum -= 10;
+			}
+
+			carry /= 10;
+			result.append(sum);
+		}
+
+		if (carry > 0) {
+			result.append(carry + sum);
+		}
+
+		BigInteger finalNumber = new BigInteger(result.toString());
+		return finalNumber;
+	}
 }
