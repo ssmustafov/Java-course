1c3b0af8560e9ee89ae276add3786a386b0d5685
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-10 13:00:47 +0300

    Refactored code

--- Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
@@ -2,14 +2,17 @@ package com.sirma.itt.javacourse.intro.hangman;
 
 import java.io.IOException;
 
+import com.sirma.itt.javacourse.intro.numbers.LargeNumbers;
+
 public class PlayHangman {
 
 	public static void main(String[] args) throws IOException {
-		String[] arr = { "developer", "java", "javascript" };
-		HangmanReader arrReader = new ArrayReader(arr);
-
-		HangmanReader fileReader = new FileReader("words.txt");
-		Hangman game = new Hangman(fileReader);
-		game.run();
+		// String[] arr = { "developer", "java", "javascript" };
+		// HangmanReader arrReader = new ArrayReader(arr);
+		//
+		// HangmanReader fileReader = new FileReader("words.txt");
+		// Hangman game = new Hangman(fileReader);
+		// game.run();
+		System.out.println(LargeNumbers.sumTwoNumbers("-5", "-7"));
 	}
 }
