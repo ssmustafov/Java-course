98744f58539d3eee815c3f4b101317f046599245
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-10 10:26:26 +0300

    Add new test for Hangman

---
+++ Intro-Java/src/test/java/com/sirma/itt/javacourse/intro/hangman/HangmanTest.java
@@ -0,0 +1,26 @@
+package com.sirma.itt.javacourse.intro.hangman;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * @author smustafov
+ */
+public class HangmanTest {
+
+	/**
+	 * Tests {@link com.sirma.itt.javacourse.intro.hangman.Hangman#isWord(java.lang.String)}.
+	 */
+	@Test
+	public void testIsWord() {
+		String[] arr = { "jdasdasd" };
+		ArrayReader reader = new ArrayReader(arr);
+
+		Hangman hangMan = new Hangman(reader);
+		if (!hangMan.isWord(arr[0])) {
+			fail("The given string is not a word");
+		}
+	}
+
+}
