dc97cd2a730028b25ddf052ab52acca4eaa2f78a
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-11 17:35:13 +0300

    Add comments

--- Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
@@ -7,19 +7,27 @@ import java.io.IOException;
  * 
  * @author smustafov
  */
-public class PlayHangman {
+public final class PlayHangman {
+
+	/**
+	 * Protects from instantiation.
+	 */
+	private PlayHangman() {
+
+	}
 
 	/**
 	 * Main method.
 	 * 
 	 * @param args
+	 *            args
 	 * @throws IOException
 	 *             throws exception if it cannot load the given file or the encode of the file is
 	 *             not UTF-8
 	 */
 	public static void main(String[] args) throws IOException {
-		String[] arr = { "developer", "java", "javascript" };
-		HangmanReader arrReader = new ArrayReader(arr);
+		// String[] arr = { "developer", "java", "javascript" };
+		// HangmanReader arrReader = new ArrayReader(arr);
 
 		HangmanReader fileReader = new FileReader("words.txt");
 		Hangman game = new Hangman(fileReader);
