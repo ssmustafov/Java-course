541a51fd16c1390853099665be2011608b2591a6
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-11 11:05:07 +0300

    Added comments

--- Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
+++ Intro-Java/src/main/java/com/sirma/itt/javacourse/intro/hangman/PlayHangman.java
@@ -2,8 +2,19 @@ package com.sirma.itt.javacourse.intro.hangman;
 
 import java.io.IOException;
 
+/**
+ * Tests the game.
+ * 
+ * @author smustafov
+ */
 public class PlayHangman {
 
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		String[] arr = { "developer", "java", "javascript" };
 		HangmanReader arrReader = new ArrayReader(arr);
