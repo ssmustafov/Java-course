fdfc00294b6dba0985acd2c942f82b582114f9f9
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-14 17:37:09 +0300

    Add sample class

---
+++ Objects-in-Java/src/main/java/com/sirma/itt/javacourse/objects/trees/Run.java
@@ -0,0 +1,40 @@
+package com.sirma.itt.javacourse.objects.trees;
+
+/**
+ * Shows how the BinaryTree class can be used.
+ * 
+ * @author smustafov
+ */
+public class Run {
+
+	public static void main(String[] args) {
+		BinaryTree binaryTree = new BinaryTree();
+		System.out.println("Binary tree:");
+		// insert new number in the tree, only if there is not already a number with same value
+		binaryTree.insert(40);
+		binaryTree.insert(78);
+		binaryTree.insert(25);
+		binaryTree.insert(10);
+		binaryTree.insert(32);
+
+		// print all elements in a sorted way
+		System.out.println("In-order sort: " + binaryTree.getInOrderSort());
+		System.out.println("Pre-order sort: " + binaryTree.getPreOrderSort());
+		System.out.println("Post-order sort: " + binaryTree.getPostOrderSort());
+
+		// search for element in the tree
+		System.out.println("Does 25 exists in the tree: " + binaryTree.doesElementExists(25));
+
+		System.out.println();
+		System.out.println("Homo tree:");
+		HomogeneousTree<Integer> homoTree = new HomogeneousTree<>(22);
+		homoTree.insert(30);
+		homoTree.insert(45);
+		homoTree.insert(4);
+		homoTree.insert(-234);
+		homoTree.insert(54);
+
+		System.out.println(homoTree.getDFS());
+
+	}
+}
