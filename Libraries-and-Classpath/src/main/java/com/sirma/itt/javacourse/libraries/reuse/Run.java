bb6a7b5cd811c5f125a896b6b96c50c35fff55f3
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-18 16:17:37 +0300

    Initial commit

---
+++ Libraries-and-Classpath/src/main/java/com/sirma/itt/javacourse/libraries/reuse/Run.java
@@ -0,0 +1,16 @@
+package com.sirma.itt.javacourse.libraries.reuse;
+/**
+ * 
+ * @author smustafov
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
