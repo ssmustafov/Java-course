591e35e9642b508a49e869fcb0d7a96d4a6a1717
Sinan Mustafov <smustafov@10.131.2.232>
2014-07-18 16:21:31 +0300

    Add comments

--- Libraries-and-Classpath/src/main/java/com/sirma/itt/javacourse/libraries/reuse/Run.java
+++ Libraries-and-Classpath/src/main/java/com/sirma/itt/javacourse/libraries/reuse/Run.java
@@ -1,12 +1,22 @@
 package com.sirma.itt.javacourse.libraries.reuse;
+
 /**
+ * Uses the code from Intro-Java project as jar libraries.
  * 
  * @author smustafov
  */
-public class Run {
+public final class Run {
+
+	/**
+	 * Default constructor.
+	 */
+	private Run() {
+
+	}
 
 	/**
 	 * @param args
+	 *            - arguments
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
