558d70c9082cdbed47214b4b7be01660a3d777fc
Sinan Mustafov <smustafov@10.131.2.232>
2014-08-04 14:45:30 +0300

    Added comments

--- Reflection-Annotations-RegEx/src/main/java/com/sirma/itt/javacourse/regex/annotations/Human.java
+++ Reflection-Annotations-RegEx/src/main/java/com/sirma/itt/javacourse/regex/annotations/Human.java
@@ -1,6 +1,9 @@
 package com.sirma.itt.javacourse.regex.annotations;
 
 /**
+ * Represents human. Implements <code>Comparable</code> interface, so the classes who extends this
+ * class can be ordered by <code>HumanAnnotation</code>.
+ * 
  * @author Sinan
  */
 public class Human implements Comparable<Human> {
