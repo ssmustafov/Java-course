a6a7b60edec02f732bcbe90c1e2ab3b3a5bb8384
Sinan Mustafov <smustafov@10.131.2.232>
2014-08-19 16:02:22 +0300

    Added new tests

--- Input-Output/src/test/java/com/sirma/itt/javacourse/inputoutput/streams/TransferObjectTest.java
+++ Input-Output/src/test/java/com/sirma/itt/javacourse/inputoutput/streams/TransferObjectTest.java
@@ -17,6 +17,24 @@ public class TransferObjectTest {
 
 	/**
 	 * Tests {@link com.sirma.itt.javacourse.inputoutput.streams.TransferObject#transfer(int, int)}
+	 * with number of bytes as zero.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testTransferWithNumberOfBytesZero() {
+		byte[] buffer = { 2, 3, 65 };
+		ByteArrayInputStream input = new ByteArrayInputStream(buffer);
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		TransferObject transfer = new TransferObject(input, output);
+
+		try {
+			transfer.transfer(0, 1);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Tests {@link com.sirma.itt.javacourse.inputoutput.streams.TransferObject#transfer(int, int)}
 	 * with negative number of bytes.
 	 */
 	@Test(expected = IllegalArgumentException.class)
@@ -73,7 +91,8 @@ public class TransferObjectTest {
 	}
 
 	/**
-	 * Tests {@link com.sirma.itt.javacourse.inputoutput.streams.TransferObject#transfer(int, int)}.
+	 * Tests {@link com.sirma.itt.javacourse.inputoutput.streams.TransferObject#transfer(int, int)}
+	 * with valid normal case.
 	 */
 	@Test
 	public void testTransfer() {
@@ -85,6 +104,28 @@ public class TransferObjectTest {
 		try {
 			int actual = transfer.transfer(100, 10);
 			int expected = 90;
+
+			assertEquals(expected, actual);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Tests {@link com.sirma.itt.javacourse.inputoutput.streams.TransferObject#transfer(int, int)}
+	 * with offset zero.
+	 */
+	@Test
+	public void testTransferWithOffsetZero() {
+		byte[] buffer = { 93, 11, 23, 65, 69, 43, 97, 66, 98, 101, 100, 5, 6, 4, 3, 67, 81, 18, 22,
+				39, 100, 65, 55, 99 };
+		ByteArrayInputStream input = new ByteArrayInputStream(buffer);
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		TransferObject transfer = new TransferObject(input, output);
+
+		try {
+			int actual = transfer.transfer(20, 0);
+			int expected = 20;
 
 			assertEquals(expected, actual);
 		} catch (IOException e) {
