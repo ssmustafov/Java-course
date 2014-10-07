fb21f4913eb45b39d2c6eb95f356e5f64280ea62
Sinan Mustafov <smustafov@10.131.2.232>
2014-10-07 18:25:45 +0300

    Add new task - Synchronized stack

---
+++ Threads/src/main/java/com/sirma/itt/javacourse/threads/synchronizedStack/Run.java
@@ -0,0 +1,23 @@
+package com.sirma.itt.javacourse.threads.synchronizedStack;
+
+/**
+ * @author smustafov
+ */
+public final class Run {
+	/**
+	 * Protects from instantiation.
+	 */
+	private Run() {
+
+	}
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 *            - arguments
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Threads/src/main/java/com/sirma/itt/javacourse/threads/synchronizedStack/SynchronizedStack.java
@@ -0,0 +1,20 @@
+package com.sirma.itt.javacourse.threads.synchronizedStack;
+
+/**
+ * Holds list of elements of type <code>Object</code> with fixed size. When the list is full it
+ * waits until an element is removed and then new one is added, and when its empty it waits until
+ * new element is added. Implements {@code Runnable} interface.
+ * 
+ * @author smustafov
+ */
+public class SynchronizedStack implements Runnable {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
